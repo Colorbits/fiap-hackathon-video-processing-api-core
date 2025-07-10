1436fedb6776d33d6fb6676ac6506bcb16828143
Gabriel Ferreira Umbelino <gabriel.f.umbelino@gmail.com>
2025-07-09 21:28:25 -0300

    fix cors error

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,16 @@ import { AllExceptionsFilter } from './shared/adapters/AllExceptionsFilter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  // Configuração CORS
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://localhost:3000'], // Frontend URLs
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // Permitir cookies
+    allowedHeaders: 'Content-Type,Accept,Authorization',
+    exposedHeaders: 'Authorization',
+  });
 
   // Adapter for handling exceptions globally
   const http_adapter = app.get(HttpAdapterHost);
